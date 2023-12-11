c58bc205366630984e065437e9c2164680994071
yazan sawan <alisarter2@gmail.com>
2023-12-11 16:39:31 +0300

    add data.json

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import main_image from "../images/mainBackground.jpg";
 import Mainman from "../images/mainMan.jpeg";
-
+import data from "../data.json";
+import { useEffect } from "react";
 const Main = () => {
+  // console.log(data);
+  // console.log(data[0].id);
+
   return (
     <div className="container page d-flex justify-content-between  align-items-center ">
       <div style={{ width: "50%" }} className="main_text p-5 ">
@@ -17,15 +21,15 @@ const Main = () => {
             height: "350px",
           }}
         />
-        <div style={{marginRight:"60px"}}>
+        <div style={{ marginRight: "60px" }}>
           <h3>السلام عليكم </h3>
-            <p>
+          <p>
             إن العلم الشرعي من اهم العلوم فتم تجميع وتلخيص جميع ابحاث الكتب
             الشرعية
           </p>
         </div>
       </div>
-      <div style={{ width: "50%", height: "30%" ,boxShadow:"0 0px 8px 1px "}}>
+      <div style={{ width: "50%", height: "30%", boxShadow: "0 0px 8px 1px " }}>
         <img width={"100%"} height={"100%"} src={main_image} alt="" />
       </div>
     </div>
