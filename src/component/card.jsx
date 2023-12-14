f13c60aeae4db2ea78f6804cf7af3b634745f38f
yazan sawan <alisarter2@gmail.com>
2023-12-14 08:58:52 +0300

    d

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -5,49 +5,55 @@ function Mycard() {
   console.log(Persondata);
   return (
     <div className="Mycard d-flex align-items-center justify-content-center flex-wrap gap-5 mb-5">
-      {Persondata.map((e) => {
-        return (
-          <Card
-            className="border shadow border-2 "
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "10px",
-              width: "20rem",
-            }}
-          >
-            <Card.Img variant="top" src={e?.img} className=" rounded-circle" />
-            <Card.Body className=" d-flex align-items-center justify-content-center text-center flex-column">
-              <Card.Title className="my-2">{e.name}</Card.Title>
-              <Card.Text className="my-2">
-                <h3 className=" d-inline-block">عمله:</h3>
-                <h4 className=" d-inline-block mx-2">{e.job}</h4>
-              </Card.Text>
-              <div className="d-flex gap-2">
-                {e.facebook ? (
-                  <Button variant="primary" className="my-2">
-                    <a href={e?.facebook} style={{ color: "#fff" }}>
-                      Facebook
-                    </a>
-                  </Button>
-                ) : (
-                  ""
-                )}
-                {e.instegram ? (
-                  <Button variant="primary" className="my-2">
-                    <a href={e?.instegram} style={{ color: "#fff" }}>
-                      Instgram
-                    </a>
-                  </Button>
-                ) : (
-                  ""
-                )}
-              </div>
-            </Card.Body>
-          </Card>
-        );
-      })}
+      {Persondata.length > 0
+        ? Persondata.map((e) => {
+            return (
+              <Card
+                className="border shadow border-2 "
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: "10px",
+                  width: "20rem",
+                }}
+              >
+                <Card.Img
+                  variant="top"
+                  src={e?.img}
+                  className=" rounded-circle"
+                />
+                <Card.Body className=" d-flex align-items-center justify-content-center text-center flex-column">
+                  <Card.Title className="my-2">{e.name}</Card.Title>
+                  <Card.Text className="my-2">
+                    <h3 className=" d-inline-block">عمله:</h3>
+                    <h4 className=" d-inline-block mx-2">{e.job}</h4>
+                  </Card.Text>
+                  <div className="d-flex gap-2">
+                    {e.facebook ? (
+                      <Button variant="primary" className="my-2">
+                        <a href={e?.facebook} style={{ color: "#fff" }}>
+                          Facebook
+                        </a>
+                      </Button>
+                    ) : (
+                      ""
+                    )}
+                    {e.instegram ? (
+                      <Button variant="primary" className="my-2">
+                        <a href={e?.instegram} style={{ color: "#fff" }}>
+                          Instgram
+                        </a>
+                      </Button>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </Card.Body>
+              </Card>
+            );
+          })
+        : ""}
     </div>
   );
 }
