9341ededcafac955f44e6634e35eda38d3f3e9ba
Ghaith <gaesx1x2@gmail.com>
2023-12-16 11:10:21 -0800

    dsa

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center bg-danger footer" style={{color:"white",height:"10vh",}}>
-      <p style={{fontSize:"20px",paddingTop:"10px"}}>تم باشراف الاستاذ : طالب مستو</p>
+      <p style={{fontSize:"20px",paddingTop:"10px"}}>تم باشراف الاستاذ : ايهاب مبارك</p>
     </div>
   );
 };
