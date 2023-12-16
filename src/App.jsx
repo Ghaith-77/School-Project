c0ff055d97891606f766e25cd74a0456d6894ced
Ghaith <gaesx1x2@gmail.com>
2023-12-16 11:12:36 -0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 import Footer from "./component/footer/footer";
 import SubjectDetails from "./pages/subjectDetails";
 import Resourses from "./pages/resourses";
-// import Person from "./pages/person";
+import Person from "./pages/person";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/resourses" element={<Resourses />} />
           <Route path="/subjectDetails/:id" element={<SubjectDetails />} />
-          {/* <Route path="/person" element={<Person />} /> */}
+          <Route path="/person" element={<Person />} />
         </Routes>
         <Footer />
       </BrowserRouter>
